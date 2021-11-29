9ce50d1f97b173c6eb6a020a17e82a005ead8bba
luiscolon0426 <luiscolon0426@gmail.com>
2021-11-29 17:16:14 +0000

    getline

---
+++ Test/getline_loop.c
@@ -0,0 +1,21 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+	char *string;
+	size_t n = 50;
+/*the condition for this while is always true
+ * because 1 is always true */
+	while (1)
+	{
+
+		string = malloc(sizeof(char) * n);
+
+		printf("$ ");
+		getline(&string, &n, stdin);
+		printf("%s", string);
+	}
+}
