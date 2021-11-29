575ed36f12a356fcd81e2820b1cb73beb60ed957
luiscolon0426 <luiscolon0426@gmail.com>
2021-11-29 17:12:19 +0000

    tokenizer

--- Test/strtok.c
+++ Test/strtok.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <string.h>
- 
+
 int main()
 {
-    char str[] ="Hello World!";
- 
-    // Returns first token
-    char *token = strtok(str, " ");
-   
-    // Keep printing tokens while one of the
-    // delimiters present in str[].
-    while (token != NULL)
-    {
-        printf("%s\n", token);
-        token = strtok(NULL, "-");
-    }
- 
-    return 0;
+	char str[] ="Hello World!";
+
+	// Returns first token
+	char *token = strtok(str, " ");
+
+	// Keep printing tokens while one of the
+	// delimiters present in str[].
+	while (token != NULL)
+	{
+		printf("%s\n", token);
+		token = strtok(NULL, "-");
+	}
+
+	return 0;
 }
