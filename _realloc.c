d43e7f6d634ed4c4f11b1f2c064da1341ca37c46
afiorg9000 <3533@holbertonschool.com>
2021-12-01 20:56:37 +0000

    fix header

--- _realloc.c
+++ _realloc.c
@@ -1,12 +1,20 @@
-void *reallocation(void *ptr,size_t size) {
+#include <stddef.h>
+
+void *reallocation(void *ptr,size_t size) 
+{
 	void *newptr;
 	int msize;
+
 	msize = getsize(ptr);
+
 	printf("msize=%d\n", msize);
+
 	if (size <= msize)
 		return ptr;
+
 	newptr = malloc(size);
 	memcpy(newptr, ptr, msize);
+
 	free(ptr);
 	return newptr;
 }
