fd2ad44ba233ee1a196f39652ca8bb5d6a770423
luiscolon0426 <luiscolon0426@gmail.com>
2021-12-01 17:32:07 +0000

    Added libraries

--- shell.h
+++ shell.h
@@ -4,5 +4,11 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <signal.h>
+
 
 #endif /* SHELL_H */
