3c26d9a62e1f59758db685be2470365ea29fd6e8
afiorg9000 <3533@holbertonschool.com>
2021-12-02 13:25:32 +0000

    void main

--- prompt.c
+++ prompt.c
@@ -1,21 +1,22 @@
 #include "sh.h"
 
-int main()
+int main(void)
 {
-    char *string;
-    size_t n = 50;
-/*the condition for this while is always true
- * because 1 is always true */
-    while (1)
-    {
+	char *string;
+	size_t n = 50;
+	
+	/**
+	 * the condition for this while is always true
+	 * because 1 is always true 
+	 */
 
-        string = malloc(sizeof(char) * n);
+	while (1)
+	{
+		string = malloc(sizeof(char) * n);
 
-        _putchar('$');
-		_putchar(' ');
-        getline(&string, &n, stdin);
-
-
-    }
+		write(STDOUT_FILENO, "$ ", 4);
+		getline(&string, &n, stdin);
+	}
 }
+
 
