a75c29258033038ac4d0d9ef36963a4fb12cc498
afiorg9000 <3533@holbertonschool.com>
2021-12-02 13:31:57 +0000

    betty

--- prompt.c
+++ prompt.c
@@ -1,13 +1,18 @@
 #include "sh.h"
 
+/**
+ * main - displays looped prompt and waits for user input.
+ * return: returns the executable argument in the command prompt line.
+ */
+
 int main(void)
 {
 	char *string;
 	size_t n = 50;
-	
+
 	/**
 	 * the condition for this while is always true
-	 * because 1 is always true 
+	 * because 1 is always true
 	 */
 
 	while (1)
