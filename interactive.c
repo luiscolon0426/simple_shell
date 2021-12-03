ae20c8ee1f1bdc0a5a9840253a2608e1f4f3938e
luiscolon0426 <luiscolon0426@gmail.com>
2021-12-03 17:48:20 +0000

    prompt loop

--- interactive.c
+++ interactive.c
@@ -1,24 +1,39 @@
 #include "sh.h"
 
-/**
- * main - displays looped prompt and waits for user input.
- * return: returns the executable argument in the command prompt line.
- */
 
-int interactive()
+int _interactive(char **av __attribute__((unused)))
 {
-	char *input;
-	size_t inputsize = 50;
-	size_t length;
+	/*@av: argument from main*/
+
+	int (*func_ptr)() = NULL;
+	char *tmp = NULL;
+	char **env_args = NULL;
+	char *complete_comm = NULL;
+	char **usr_comm = NULL;
+	size_t tmpsize;
+
+
 
 	while (1)
 	{
-		input = malloc(sizeof(char) * inputsize);
-
 		write(STDOUT_FILENO, "($) ", 4);
-		length = getline(&input, &inputsize, stdin);
-		input[length - 1] = '\0';
+		getline(&tmp, &tmpsize, stdin);
+		validategetline(tmp, 0, usr_comm, env_args);
 
+		if (tmp[0] == 10 || tmp[0] == 9)
+			continue;
+		usr_comm = args_constructor(tmp);
 
+		if (usr_comm == NULL)
+			continue;
+
+		func_ptr = find_builtins(*usr_comm);
+
+		if (func_ptr)
+		{
+			if (func_ptr == exit_func)
+				free_all(usr_comm, env_)
+		}
 	}
 }
+
