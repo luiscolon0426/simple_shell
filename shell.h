be02584891580300e5a00e16a10f29df04d401a0
afiorg9000 <3533@holbertonschool.com>
2021-12-03 19:13:28 +0000

    header

---
+++ shell.h
@@ -0,0 +1,17 @@
+#ifndef SH_H
+#define SH_H
+
+/* LIBRARIES */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <signal.h>
+
+
+/* PROTOTYPES */
+#endif /* SH_H */
