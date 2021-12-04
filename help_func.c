0ed814bbe1f7b922546f756df3810227b2c91c42
luiscolon0426 <luiscolon0426@gmail.com>
2021-12-04 17:05:44 +0000

    Added functions

--- help_func.c
+++ help_func.c
@@ -1,1 +1,29 @@
 #include "shell.h"
+
+/**
+ * _exit_func - when exiting the shell,
+ * return 0
+ *
+ * Return: Always 0
+ */
+int _exit_func(void)
+{
+	return (0);
+}
+
+int _str_cmp(char *str1, char *str2)
+{
+	int idxa, idxb;
+	char space = (' ');
+
+	for (idxa = 0, idxb = 0; str1[idxa] != '\0' || str2[idxb] != '\0'; idxa++, idxb++)
+	{
+		while (str1[idxa] == space)
+		{
+			idxa++;
+		}
+		if (str1[idxa] != str2[idxb])
+			return (0);
+	}
+	return (1);
+}
