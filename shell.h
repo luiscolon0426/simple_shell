cbeb4d5afe47474d761c32588c2fbd79ff08d26c
afiorg9000 <3533@holbertonschool.com>
2021-12-04 15:20:12 +0000

    protype

--- shell.h
+++ shell.h
@@ -14,11 +14,5 @@
 
 
 /* PROTOTYPES */
-<<<<<<< HEAD
 int main (int argc, char **argv, char **env);
-#endif /* SH_H */
-=======
-
-
 #endif /* SHELL_H */
->>>>>>> 3d8d4a213dbbba3c5cc6d334a9fe506409d1d32a
