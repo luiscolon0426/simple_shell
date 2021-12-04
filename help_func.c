fd5a275773132701c13858ab54b20a6e0293d2ec
afiorg9000 <3533@holbertonschool.com>
2021-12-04 19:31:10 +0000

    func

--- help_func.c
+++ help_func.c
@@ -21,6 +21,42 @@ int _strlen(char *str)
  */
 char **_strtok(char *str)
 {
+char *token = NULL;
+char **arg_str = NULL;
+int idx, count = 0, len = 0;
+
+for (idx = 0; str[idx] != '\0'; idx++)
+	{
+		if (str[idx] == ' ')
+		{
+			count++;
+		}
+	}
+	count += 2;
+
+	arg_str = malloc(sizeof(char *) * count);
+	if (arg_str == NULL)
+	{
+		return (NULL);
+	}
+	token = strtok(string, " ");
+
+	for (idx = 0; token != NULL; idx++)
+	{
+		len = _strlen(token);
+		arg_str[idx] = malloc(sizeof(char) * (len + 1));
+		if (arg_str[idx] == NULL)
+		{
+			return (NULL);
+		}
+		arg_str[idx] = token;
+		token = strtok(NULL, " ");
+	}
+	arg_str[idx] = NULL;
+	return (arg_str);
+}
+
+
 /**
  * _strcmp - compares two string.
  * @str1: first string to compare.
@@ -44,3 +80,6 @@ int _strcmp(char *str1, char *str2)
 	}
 	return (1);
 }
+
+/**
+ * _strcpy - Cop
