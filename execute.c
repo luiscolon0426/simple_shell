9aab2befb534471aeb92c7837f4e1bd5668f682b
luiscolon0426 <luiscolon0426@gmail.com>
2021-12-06 17:43:52 +0000

    Betty friendly

--- execute.c
+++ execute.c
@@ -1,24 +1,24 @@
 #include "shell.h"
 
 /**
- * _execute - checks for errors before launching the command
- * @args: string with commands
+ * _executable - checks for errors before launching the command
+ * @str_com: string with commands
  * Return: 1 if launch was success
  */
 int _executable(char **str_com)
 {
-    struct stat st;
+	struct stat st;
 
-    if (str_com[0] == NULL)
-        return (1);
+	if (str_com[0] == NULL)
+		return (1);
 
-    else if (strcmp(str_com[0], "printenv") == 0)
-    {
-        environment();
-        return(1);
-    }
+	else if (strcmp(str_com[0], "printenv") == 0)
+	{
+		environment();
+		return (1);
+	}
 
-    else if (strcmp(str_com[0], "exit") == 0)
-        exit(EXIT_SUCCESS);
-    return (_builtin_function(str_com));
+	else if (strcmp(str_com[0], "exit") == 0)
+		exit(EXIT_SUCCESS);
+	return (_launch(str_com));
 }
