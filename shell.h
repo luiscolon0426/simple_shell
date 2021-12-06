4c082c86d6a330dad02a383334793ef029f250dc
luiscolon0426 <luiscolon0426@gmail.com>
2021-12-06 17:44:15 +0000

    Betty friendly

--- shell.h
+++ shell.h
@@ -1,8 +1,11 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+
 extern char **environ;
 
+
+/* PROTOTYPES */
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -15,12 +18,24 @@ extern char **environ;
 #include <errno.h>
 #include <linux/limits.h>
 
+
+
+/**
+ * struct builtins - allows user to provide
+ * alternative names for the primitive user
+ * defined
+ * @str1: string One
+ * @str2: string 2
+ **/
 typedef struct builtins
 {
 char *str1;
 char *str2;
 } builtin;
 
+
+
+/* PROTOTYPES */
 char *_user_input(void);
 char **_str_tok(char *string);
 int _executable(char **str_com);
@@ -30,5 +45,6 @@ int str_len(char *str);
 char *_strcpy(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
 int _builtin_function(char **args);
+
 
 #endif /* SHELL_H */
