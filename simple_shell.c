c0887c2f3c3e4a6454ee256e0aab1215b4dca4e3
luiscolon0426 <luiscolon0426@gmail.com>
2021-12-08 18:15:20 +0000

    task 2: check 2

--- simple_shell.c
+++ simple_shell.c
@@ -1,5 +1,5 @@
 #include "shell.h"
-#include <unistd.h>
+
 
 /**
  * main - Entry point
@@ -12,16 +12,14 @@ int main(void)
 	char **str_com;
 	int flag;
 
-/*need to add environ, but idk where*/
+	/*need add environ, but idk where*/
 
 	do {
 		flag = isatty(STDIN_FILENO);
 		write(1, "(#cisfun$) ", 11);
 		string = _user_input();
 		str_com = _str_tok(string);
-		_executable(str_com);
-		free(string);
-		free(str_com);
+		_executable(str_com, string);
 	} while (flag);
 	return (EXIT_SUCCESS);
 }
