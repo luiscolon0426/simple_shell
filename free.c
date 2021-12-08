2a72d6af2d67ca72dba01b20f906ab81e03d6ab5
luiscolon0426 <luiscolon0426@gmail.com>
2021-12-08 21:04:52 +0000

    fixing leaks

--- free.c
+++ free.c
@@ -1,14 +1,13 @@
 #include "shell.h"
-
 /**
  * free_grid - frees a 2D array
  * @args: arguments
  */
 void free_grid(char **args)
 {
-    int i = 0;
+	int i = 0;
 
-    while (!args)
-        free(args[i++]);
-    free(args);
+	while (!args)
+		free(args[i++]);
+	free(args);
 }
