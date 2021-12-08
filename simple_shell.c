e51aeb860f9b74c7b98a6a0f8110bb4729c296da
luiscolon0426 <luiscolon0426@gmail.com>
2021-12-08 03:05:06 +0000

    checker 2

--- simple_shell.c
+++ simple_shell.c
@@ -1,4 +1,5 @@
 #include "shell.h"
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -9,16 +10,18 @@ int main(void)
 {
 	char *string;
 	char **str_com;
+	int flag;
 
 /*need to add environ, but idk where*/
 
 	do {
+		flag = isatty(STDIN_FILENO);
 		write(1, "(#cisfun$) ", 11);
 		string = _user_input();
 		str_com = _str_tok(string);
 		_executable(str_com);
 		free(string);
 		free(str_com);
-	} while (1);
+	} while (flag);
 	return (EXIT_SUCCESS);
 }
